e5cc6d29e88bceae83a356cf42eb4cbc66368d28
Ilia <ila04112007@gmail.com>
2025-02-15 11:52:11 +0300

    добавил методы Activity

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -33,5 +36,42 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }}
+    }
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if (savedInstanceState != null &&
+                savedInstanceState.containsKey("counter")) {
+            counter = savedInstanceState.getInt("counter");
+        }
+        Log.d("Instance", "onRestoreInstanceState");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle autState){
+        super.onSaveInstanceState(autState);
+        autState.putInt("Нажалось", counter);
+        Log.d("Instens","onSave");
+        Toast.makeText(this, "onSave",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected  void onStart(){
+        super.onStart();
+        Log.d("Instens", "onStart");
+        Toast.makeText(this, "onStart", Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected  void onResume(){
+        super.onResume();
+        Log.d("Instens", "onResume");
+        Toast.makeText(this, "onResume", Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected  void onPause(){
+        super.onPause();
+        Log.d("Instens", "onPause");
+        Toast.makeText(this, "onPause", Toast.LENGTH_LONG).show();
+    }
+}
 
